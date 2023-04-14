f924bad4606c1147413d79660246999dba6941ad
shrutiJethwa <77672199+shrutiJethwa@users.noreply.github.com>
2023-04-14 17:25:12 +0530

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: guess(),
+    );
+  }
+}
+
+class guess extends StatefulWidget {
+  const guess({Key? key}) : super(key: key);
+
+  @override
+  State<guess> createState() => _guessState();
+}
+
+class _guessState extends State<guess> {
+  int num=1;
+  void change()
+  {
+    setState(() {
+      num=Random().nextInt(5)+1;
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body:  Center(
+        child: TextButton(
+
+          onPressed: () {
+            change();
+          },
+          child: Image(
+
+              image: AssetImage("images/ball$num.png"),
+            ),
+        ),
+      ),
+
+    );
+  }
+}
+
